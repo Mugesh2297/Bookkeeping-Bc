e243fff0e9e6f965509bb532d73aaef8991e68e0
Mugesh Kumar K <105528383+Mugesh2297@users.noreply.github.com>
2022-12-12 00:11:57 +0530

    Update sellerRouter.js

--- routes/sellerRouter.js
+++ routes/sellerRouter.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const Seller = require("../models/sellerModels");
 const { default: mongoose } = require("mongoose");
 const ObjectId = mongoose.Types.ObjectId;
-const auth = require("../modules/auth");
+const auth = require("../Modules/auth");
 
 
 router.post("/createSeller", auth.authenticateUser, async function (req, res) {
