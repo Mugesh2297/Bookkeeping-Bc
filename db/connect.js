515141fb097288309d5072917bd5f423d256989f
Mugesh Kumar K <105528383+Mugesh2297@users.noreply.github.com>
2022-12-13 14:46:14 +0530

    Update connect.js

--- db/connect.js
+++ db/connect.js
@@ -1,9 +1,11 @@
 
 const mongoose = require('mongoose');
 require('dotenv').config();
+mongoose.set('strictQuery', true);
+
 let Link = process.env.MONGO_URL
 
-mongoose.connect(`${Link}`,{ useNewUrlParser: true,useUnifiedTopology: true}).then(() => {
+mongoose.connect(`${Link}`).then(() => {
     console.log("DB Connected Done");
 }).catch((error) => {
     console.log(error);
